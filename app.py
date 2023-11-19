c5b60e179f1e7e908e20fe512c453b90041dfbdd
Husny Ahamed MG <Husny.Ahmed@fleetcor.com>
2023-11-19 13:04:30 +0100

    if statements

--- app.py
+++ app.py
@@ -5,7 +5,18 @@
 # summ = input_1 + input_2
 # print("Sum: " + str(summ))
 
-print(not  3 > 4)
-print (10 > 20 or 10 > 9)
-print (10 > 20 and 10 > 9)
+# print(not  3 > 4)
+# print (10 > 20 or 10 > 9)
+# print (10 > 20 and 10 > 9)
 
+# if 36 < temp < 38:
+temp = float(input("Temperature: "))
+
+if temp > 36 and temp < 38:
+    print("Normal")
+elif temp < 36: # this is a comment, too low temp
+    print("Hyperthermia")
+else:
+    print("Fever")
+
+print("done!")
