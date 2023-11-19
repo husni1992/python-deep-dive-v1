04b3bdb36b655a324beed83cffd6dc118e1625f1
Husny Ahamed MG <Husny.Ahmed@fleetcor.com>
2023-11-19 15:03:54 +0100

    topples

---
+++ exercise_4.py
@@ -0,0 +1,9 @@
+# toples
+NAMES_OF_COUNTRIES = ("SriLanka", "Qatar", "Netherlands", "SriLanka" ,"Italy")
+
+# Another tuple
+MORE_COUNTRIES = ("France", "Germany")
+
+COMBINED_COUNTRIES = NAMES_OF_COUNTRIES.__add__(MORE_COUNTRIES)
+
+print(COMBINED_COUNTRIES)
