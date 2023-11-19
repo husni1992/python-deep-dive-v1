7827797d4a4f01778e8b80eb402619d67373b42f
Husny Ahamed MG <Husny.Ahmed@fleetcor.com>
2023-11-19 12:47:34 +0100

    logical operators

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-input_1 = float(input("enter no 1: "))
-input_2 = float(input("enter no 2: "))
-summ = input_1 + input_2
-print("Sum: " + str(summ))
+# print('Hello world!')
+#
+# input_1 = float(input("enter no 1: "))
+# input_2 = float(input("enter no 2: "))
+# summ = input_1 + input_2
+# print("Sum: " + str(summ))
+
+print(not  3 > 4)
+print (10 > 20 or 10 > 9)
+print (10 > 20 and 10 > 9)
+
